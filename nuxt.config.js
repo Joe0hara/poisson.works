9c6770a2846d9e6caf2d14eb57bd7c607f903634
joe0hara <joe.spyair@gmail.com>
2022-02-08 14:39:48 +0900

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   // target: 'static',
+  target: 'serverless',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
